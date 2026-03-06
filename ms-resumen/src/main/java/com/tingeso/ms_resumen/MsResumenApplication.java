6e106bce6c505e93b698d7789e562d0dd5244f45
German Peralta Huaquiñir <german.peralta@usach.cl>
2026-03-06 19:55:01 -0300

    BASE. Cimientos

---
+++ ms-resumen/src/main/java/com/tingeso/ms_resumen/MsResumenApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.ms_resumen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsResumenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsResumenApplication.class, args);
+	}
+
+}
---
+++ ms-salidas/src/main/java/com/tingeso/ms_salidas/MsSalidasApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.ms_salidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSalidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSalidasApplication.class, args);
+	}
+
+}
