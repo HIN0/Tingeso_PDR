ad20e7b7e1a627b23dc0e715a8de9c38baf84245
German Peralta Huaquiñir <german.peralta@usach.cl>
2026-03-07 12:17:08 -0300

    BACK. Corrección de tipado.

--- api-gateway/src/test/java/com/tingeso/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/tingeso/api_gateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tingeso.api_gateaway;
+package com.tingeso.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGateawayApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
